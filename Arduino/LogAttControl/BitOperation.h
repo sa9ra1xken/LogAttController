a35e8ce59711bac3e871f22c539353bbfec26551
sa9ra1xken <sa9ra1xken@gmail.com>
2025-09-08 08:37:04 +0900

    first working version
    Signed-off-by: sa9ra1xken <sa9ra1xken@gmail.com>

---
+++ Arduino/LogAttControl/BitOperation.h
@@ -0,0 +1,20 @@
+byte reverseByte(byte b) {
+  byte reversedB = 0;
+  for (int i = 0; i < 8; i++) {
+    if (bitRead(b, i)) { // Check if the i-th bit is set
+      bitWrite(reversedB, 7 - i, 1); // Set the corresponding bit in the reversed byte
+    }
+  }
+  return reversedB;
+}
+
+byte swapBits(byte b, int pos1, int pos2){
+  byte mask1 = 0x01 << pos1; 
+  byte mask2 = 0x01 << pos2;
+  bool source1 = b & mask1;   
+  bool source2 = b & mask2;
+  byte dest1 = source2 ? mask1 : 0x00;
+  byte dest2 = source1 ? mask2 : 0x00;
+  byte unchanged = b & ~ ( mask1 | mask2 ) ;
+  return unchanged | dest1 | dest2; 
+}
--- Arduino/LogAttControl/LogAttControl.ino
+++ Arduino/LogAttControl/LogAttControl.ino
@@ -1,66 +1,230 @@
 #include <Wire.h>
-#include <Adafruit_GFX.h>         //Adarfuitの画像描写ライブラリーを読み込む
-#include <Adafruit_SSD1306.h>     //AdarfuitのSSD1306用ライブラリーを読み込む
-
+#include <Adafruit_GFX.h>         //Adarfuitの画像描写ライブラリー
+#include <Adafruit_SSD1306.h>     //AdarfuitのSSD1306用ライブラリー
 #include <Arduino.h>
 #include <RotaryEncoder.h>
-
-#define RE_IN2 A2
-#define RE_IN1 A3
-
-// A pointer to the dynamic created rotary encoder instance.
-// This will be done in setup()
-RotaryEncoder *encoder = nullptr;
-
-void checkPosition()
-{
-  encoder->tick(); // just call tick() to check the state.
-}
+#include <SPI.h>
+#include "BitOperation.h"
 
 const int SCREEN_WIDTH = 128;     //ディスプレイのサイズ指定
 const int SCREEN_HEIGHT = 64;     //ディスプレイのサイズ指定
 const int SCREEN_ADDRESS = 0x3C;  //I2Cのアドレス指定
+Adafruit_SSD1306 display( SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
 
-Adafruit_SSD1306 display( SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);  //ディスプレイ制御用のインスタンスを作成。この時にデイスプレのサイズを渡す。
+#define RE_IN1 2
+#define RE_IN2 3
+#define BUTTON_IN 7
+RotaryEncoder *encoder = nullptr;
+bool LAST_BUTTON_STATUS;
 
-int count = 1;                    //countを整数型の変数として定義
+SPISettings mySPISettings = SPISettings(8000000, LSBFIRST, SPI_MODE0);
+
+enum Mode{
+  VOLUME,
+  BALANCE,
+  FF00,
+};
+
+Mode myMode; 
+int volume;
+int balance;
+int ff00;
+
+void checkPosition()
+{
+  encoder->tick();
+}
+
+void ButtonPressed(){
+}
+
+void ButtonReleased(){
+  switch(myMode){
+    case VOLUME:
+      myMode = BALANCE;
+      encoder->setPosition(balance);
+    break;  
+    case BALANCE:
+      myMode = FF00;
+      encoder->setPosition(ff00);
+    break;  
+    case FF00:
+      myMode = VOLUME;
+      encoder->setPosition(volume);
+    break;  
+  };
+  Show();
+}
+
+void WriteHeader(String str){
+      display.setCursor(0, 0);
+      display.setTextSize( 2 );
+      display.print(str);
+}
+
+void WriteBody(String str){
+      display.setCursor(0, 20);
+      display.setTextSize( 3 );
+      display.print(str);
+}
+
+void WriteFooter(String str){
+      display.setCursor(0, 48);
+      display.setTextSize( 2 );
+      display.print(str);
+}
+
+void SendAttenuaion(){
+
+  int left = volume;
+  int right = volume;
+
+  if (balance < 0) right += balance; 
+  if (balance > 0) left -= balance;
+
+  if (left < -255) left = -255; 
+  if (right < -255) right = -255;
+
+  byte data_right = swapBits(-right, 5, 7); 
+  byte data_left = swapBits(reverseByte(-left), 0, 2);
+
+  Serial.print( data_left );
+  Serial.print("/");
+  Serial.println( data_right );
+
+  SPI.beginTransaction(mySPISettings);
+  digitalWrite(SS, LOW);
+  SPI.transfer(~data_right);
+  SPI.transfer(~data_left);
+    
+  digitalWrite(SS, HIGH);
+  SPI.endTransaction();
+
+}
+
+void SendFF00(){
+
+  int data = (ff00 == 0) ? 0 : 0xff;
+
+  SPI.beginTransaction(mySPISettings);
+  digitalWrite(SS, LOW);
+  SPI.transfer(data);
+  SPI.transfer(data);
+  digitalWrite(SS, HIGH);
+  SPI.endTransaction();
+
+}
+
+void Show(){
+  display.clearDisplay();
+  display.setTextColor( SSD1306_WHITE, SSD1306_BLACK);
+  switch(myMode){
+
+    case VOLUME:
+      WriteHeader("Volume");
+      WriteBody(String(volume * 0.2, 1)+"dB");
+    break;  
+
+    case BALANCE:
+      if (balance < 0){
+        WriteHeader("Right is");
+        WriteBody(String(abs(balance) * 0.2, 1)+"dB");
+        WriteFooter("lower");
+      }
+      else if (balance > 0){
+        WriteHeader("Left is");
+        WriteBody(String(balance * 0.2, 1)+"dB");
+        WriteFooter("lower");
+      }
+      else{
+        WriteHeader("Balance");
+        WriteBody(String(0.0, 1)+"dB");
+      }
+    break;
+
+    case FF00:
+        WriteHeader("FF00");
+        WriteBody(String(ff00 & 0x1));
+    break;
+
+  };
+  display.display();
+}
+
+void VolumeChanged(){
+  if (volume > 0){volume = 0; encoder->setPosition(volume);}
+  if (volume < -255){volume = -255; encoder->setPosition(volume);}
+  Show();
+  SendAttenuaion();
+}
+
+void BalanceChanged(){
+  if (balance > 50){balance = 50; encoder->setPosition(balance);}
+  if (balance < -50){balance = -50; encoder->setPosition(balance);}
+  Show();
+  SendAttenuaion();
+}
+
+void FF00Changed(){
+  if (ff00 > 1){ff00 = 1; encoder->setPosition(ff00);}
+  if (ff00 < 0){ff00 = 0; encoder->setPosition(ff00);}
+  Show();
+  SendFF00();
+}
 
 void setup() {
+  
+  Serial.begin( 9600 );
+
   display.begin( SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS );
   display.clearDisplay();
   display.display();
 
   pinMode(RE_IN1, INPUT_PULLUP);
   pinMode(RE_IN2, INPUT_PULLUP);
-
-  // setup the rotary encoder functionality
-
-  // use FOUR3 mode when PIN_IN1, PIN_IN2 signals are always HIGH in latch position.
-  // encoder = new RotaryEncoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::FOUR3);
-
-  // use FOUR0 mode when PIN_IN1, PIN_IN2 signals are always LOW in latch position.
-  // encoder = new RotaryEncoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::FOUR0);
-
-  // use TWO03 mode when PIN_IN1, PIN_IN2 signals are both LOW or HIGH in latch position.
   encoder = new RotaryEncoder(RE_IN1, RE_IN2, RotaryEncoder::LatchMode::TWO03);
 
-  // register interrupt routine
+  pinMode(BUTTON_IN, INPUT_PULLUP);
+  LAST_BUTTON_STATUS = digitalRead(BUTTON_IN);
+
+  myMode = VOLUME;
+  encoder->setPosition(volume);
+  Show();
+
   attachInterrupt(digitalPinToInterrupt(RE_IN1), checkPosition, CHANGE);
   attachInterrupt(digitalPinToInterrupt(RE_IN2), checkPosition, CHANGE);
-} 
+  
+  SPI.begin();
+ } 
 
 void loop() {
-  static int pos = 0;
-  encoder->tick(); // just call tick() to check the state.
-  int newPos = encoder->getPosition();
-  if (pos != newPos) {
-    display.clearDisplay();
-    display.setTextSize( 3 );
-    display.setTextColor( SSD1306_WHITE, SSD1306_BLACK);
-    display.setCursor(0, 0);
-    display.print(String(newPos*0.2,1));
-    display.print("dB");
-    display.display();
-    pos = newPos;
+  delay(10);
+  bool BUTTON_STATUS = digitalRead(BUTTON_IN);  
+  if (BUTTON_STATUS != LAST_BUTTON_STATUS){
+    if (BUTTON_STATUS == LOW) ButtonPressed();
+    else ButtonReleased();
   }
+  LAST_BUTTON_STATUS = BUTTON_STATUS; 
+
+  int newPos = encoder->getPosition();
+  switch (myMode){
+    case VOLUME:
+      if (newPos != volume){
+        volume = newPos;
+        VolumeChanged();
+      }
+      break;
+    case BALANCE:
+      if (newPos != balance){
+        balance = newPos;
+        BalanceChanged();
+      }
+      break;
+    case FF00:
+      if (newPos != ff00){
+        ff00 = newPos;
+        FF00Changed();
+      }
+      break;
+  } 
 }
